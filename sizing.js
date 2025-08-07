0954257b16f363d1a00741722ae583e5ef5e9981
santiagobenitez65 <santiagobenitez652005@gmail.com>
2025-08-06 22:47:49 -0300

    Arreglé sizing.js

--- sizing.js
+++ sizing.js
@@ -1,11 +1,19 @@
-let vpHeight = document.documentElement.clientHeight;
-let vpWidth = document.documentElement.clientWidth;
+let vp = document.documentElement;
 const body = document.body;
 const h1 = document.getElementsByTagName("h1")
 
-if (vpHeight < vpWidth) {
-    body.style.fontSize = "5vh" 
-    h1[0].style.fontSize = "5vh"
-} else {
-    body.style.fontSize = "4vw" 
+function changeScale() {
+    if (vp.clientHeight < vp.clientWidth) {
+        body.style.fontSize = "5vw" 
+        h1[0].style.fontSize = "5vw"
+    } else {
+        body.style.fontSize = "7vh" 
+        h1[0].style.fontSize = "5vh"
+    }
 }
+
+changeScale()
+window.addEventListener("resize", function() {
+    console.log("resize");
+    changeScale();
+});
